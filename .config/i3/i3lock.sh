1a0d14f9ebc08866492ecb187b5baeb6bf76526e
Alfredo Pérez <alfredoperez1998@protonmail.com>
2021-06-17 22:54:57 -0500

    delete unused files

--- .config/i3/i3lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-scrot -q 100 /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-composite -gravity center ~/Pictures/Gentoo-logo-dark.png /tmp/screen.png /tmp/screen.png
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
